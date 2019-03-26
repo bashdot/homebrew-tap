67aa8a7ce910387e1ecf91035bdbc0409d31055b
Brett W <brett9000@icloud.com>
2019-03-26 06:23:10 -0700

    updated sha

--- Formula/bashdot.rb
+++ Formula/bashdot.rb
@@ -3,7 +3,7 @@ class Bashdot < Formula
   homepage "https://github.com/bashdot/bashdot"
   url "https://github.com/bashdot/bashdot/archive/2.1.0.tar.gz"
   version "2.1.0"
-  sha256 "f2a294ce64a8dadcbb8907300c2ee87a45bb5f2fb8bef875e0dbe9b58b74c18e"
+  sha256 "d0a65dc33a7024fedd32fd626e3764f0bb0d2e89572bfeb45b5266e9dea7a73a"
 
   def install
     bin.install "bashdot"
