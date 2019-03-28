ed892a6431106f0cbfb0772e735cc8ebaa89317a
Brett W <brett9000@icloud.com>
2019-03-27 20:46:16 -0700

    update to 3.0.0

--- Formula/bashdot.rb
+++ Formula/bashdot.rb
@@ -1,8 +1,8 @@
 class Bashdot < Formula
   desc "Dotfile management framework with support for multiple profiles, providing different configurations for different environments.  100% bash."
   homepage "https://github.com/bashdot/bashdot"
-  url "https://github.com/bashdot/bashdot/archive/2.1.0.tar.gz"
-  version "2.1.0"
+  url "https://github.com/bashdot/bashdot/archive/3.0.0.tar.gz"
+  version "3.0.0"
   sha256 "d0a65dc33a7024fedd32fd626e3764f0bb0d2e89572bfeb45b5266e9dea7a73a"
 
   def install
