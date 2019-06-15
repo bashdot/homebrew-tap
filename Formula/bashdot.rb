62124a84d52706fd39498c86635fee0b5ecf145e
Brett W <brett9000@icloud.com>
2019-06-15 15:41:24 -0700

    updated comment

--- Formula/bashdot.rb
+++ Formula/bashdot.rb
@@ -1,5 +1,8 @@
+# To get SHA for new version:
+#
 # wget https://github.com/bashdot/bashdot/archive/VERSION.tar.gz
 # shasum -a 256 VERSION.tar.gz
+# 
 class Bashdot < Formula
   desc "Dotfile management framework with support for multiple profiles, providing different configurations for different environments.  100% bash."
   homepage "https://github.com/bashdot/bashdot"
