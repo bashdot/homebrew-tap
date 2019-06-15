b9556ecd79f16c3eef14cf8314a91d37dad87a97
Brett W <brett9000@icloud.com>
2019-06-15 15:35:37 -0700

    updated version

--- Formula/bashdot.rb
+++ Formula/bashdot.rb
@@ -1,9 +1,11 @@
+# wget https://github.com/bashdot/bashdot/archive/VERSION.tar.gz
+# shasum -a 256 VERSION.tar.gz
 class Bashdot < Formula
   desc "Dotfile management framework with support for multiple profiles, providing different configurations for different environments.  100% bash."
   homepage "https://github.com/bashdot/bashdot"
-  url "https://github.com/bashdot/bashdot/archive/4.1.2.tar.gz"
-  version "4.1.2"
-  sha256 "24327c6c540d7fb3fe1916b450fc881f7a13242bbc52fb24fedafa94b82e874a"
+  url "https://github.com/bashdot/bashdot/archive/4.1.3.tar.gz"
+  version "4.1.3"
+  sha256 "c4c319400e1a830f5a2639987b6427f51be760a3383ce96c969ab610a330c8dc"
 
   def install
     bin.install "bashdot"
