c1d0ceffa808108bfc32dbfdf639ee20bed3f4bd
Brett W <brett9000@icloud.com>
2021-03-20 07:22:22 -0700

    releasing 4.1.4

--- Formula/bashdot.rb
+++ Formula/bashdot.rb
@@ -6,9 +6,9 @@
 class Bashdot < Formula
   desc "Dotfile management framework with support for multiple profiles, providing different configurations for different environments.  100% bash."
   homepage "https://github.com/bashdot/bashdot"
-  url "https://github.com/bashdot/bashdot/archive/4.1.3.tar.gz"
-  version "4.1.3"
-  sha256 "3306a0d7151d7b0b2774ebd64d687d29f5d118ea1e055a6f71db9f885a07d7ee"
+  url "https://github.com/bashdot/bashdot/archive/4.1.4.tar.gz"
+  version "4.1.4"
+  sha256 "8ba4dfadbf40e4af4fca48b9cf6ce7b8507ab30a763963756fec02bdf6773c4f"
 
   def install
     bin.install "bashdot"
