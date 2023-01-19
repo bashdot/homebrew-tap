138c495bc2373ab8c3fe41d64434f0a813217dec
Brett W <brett9000@icloud.com>
2023-01-18 16:09:44 -0800

    update to 4.1.7

--- Formula/bashdot.rb
+++ Formula/bashdot.rb
@@ -6,9 +6,9 @@
 class Bashdot < Formula
   desc "Dotfile management framework with support for multiple profiles, providing different configurations for different environments.  100% bash."
   homepage "https://github.com/bashdot/bashdot"
-  url "https://github.com/bashdot/bashdot/archive/4.1.6.tar.gz"
-  version "4.1.6"
-  sha256 "bd630c4921653e493ced24d78805262f31182fe4e4e4d7385a0ff4c054a62876"
+  url "https://github.com/bashdot/bashdot/archive/4.1.7.tar.gz"
+  version "4.1.7"
+  sha256 "61e981f825078b784a28d62e51a5c774adb27523d386bed80f103b83e3f8248e"
 
   def install
     bin.install "bashdot"
